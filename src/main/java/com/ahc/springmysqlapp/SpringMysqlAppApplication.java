36fb6c87d4a89444851f01d0a881e637c33e3992
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-12-11 17:58:43 +0530

    Initial commit.

---
+++ src/main/java/com/ahc/springmysqlapp/SpringMysqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.ahc.springmysqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahc/springmysqlapp/SpringMysqlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahc.springmysqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
