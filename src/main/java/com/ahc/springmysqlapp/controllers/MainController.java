d3ce55d07c7499697e3c0eee28538e69a046f1a4
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-12-11 18:49:41 +0530

    Add Get and Post mapping without web interface.

---
+++ src/main/java/com/ahc/springmysqlapp/controllers/MainController.java
@@ -0,0 +1,35 @@
+package com.ahc.springmysqlapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ahc.springmysqlapp.domain.User;
+import com.ahc.springmysqlapp.repositories.UserRepository;
+
+@Controller
+@RequestMapping("/demo")
+public class MainController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping("/add")
+	public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+		User user = new User();
+		user.setName(name);
+		user.setEmail(email);
+		userRepository.save(user);
+		return "Saved";
+	}
+
+	@GetMapping("/all")
+	public @ResponseBody Iterable<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+}
