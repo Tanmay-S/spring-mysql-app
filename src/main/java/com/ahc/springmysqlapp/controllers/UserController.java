f88ad1eb84d7b85edce1b2b7939857cfff4cbb34
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-12-15 02:08:20 +0530

    Added web interface to add and display new user

--- src/main/java/com/ahc/springmysqlapp/controllers/UserController.java
+++ src/main/java/com/ahc/springmysqlapp/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.ahc.springmysqlapp.controllers;
 
+import com.ahc.springmysqlapp.domain.User;
 import com.ahc.springmysqlapp.repositories.UserRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -22,9 +26,29 @@ public class UserController {
     return "user/index";
   }
 
-  @RequestMapping("/new")
+  @GetMapping("/register")
   public String getUserRegistrationPage() {
-    return "user/new";
+    return "user/register";
   }
-  
+
+  /**
+   * Register new user into database.
+   * 
+   * @param name  is user's name
+   * @param email is user's email
+   * @return Spring View
+   */
+  @PostMapping("/register")
+  public String userSubmit(@RequestParam String name, @RequestParam String email, Model model) {
+
+    User user = new User();
+    user.setName(name);
+    user.setEmail(email);
+    userRepository.save(user);
+
+    model.addAttribute("name", name);
+    model.addAttribute("email", email);
+    return "user/register-result";
+  }
+
 }
