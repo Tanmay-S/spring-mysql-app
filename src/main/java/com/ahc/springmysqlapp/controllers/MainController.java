6868286e97362d0feb5497192bf14839f9fdc57c
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-12-14 17:43:04 +0530

    Added a demo Javadoc

--- src/main/java/com/ahc/springmysqlapp/controllers/MainController.java
+++ src/main/java/com/ahc/springmysqlapp/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.ahc.springmysqlapp.controllers;
 
+import com.ahc.springmysqlapp.domain.User;
+import com.ahc.springmysqlapp.repositories.UserRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,32 +10,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ahc.springmysqlapp.domain.User;
-import com.ahc.springmysqlapp.repositories.UserRepository;
-
 @Controller
 @RequestMapping("/demo")
 public class MainController {
 
-	private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-	public MainController(UserRepository userRepository) {
-		super();
-		this.userRepository = userRepository;
-	}
+  public MainController(UserRepository userRepository) {
+    super();
+    this.userRepository = userRepository;
+  }
 
-	@PostMapping("/add")
-	public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
-		User user = new User();
-		user.setName(name);
-		user.setEmail(email);
-		userRepository.save(user);
-		return "Saved";
-	}
+  /**
+   * Add new user to database.
+   * 
+   * @param name  will add user name
+   * @param email will add user e-mail
+   * @return response body
+   */
+  @PostMapping("/add")
+  public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+    User user = new User();
+    user.setName(name);
+    user.setEmail(email);
+    userRepository.save(user);
+    return "Saved";
+  }
 
-	@GetMapping("/all")
-	public @ResponseBody Iterable<User> getAllUsers() {
-		return userRepository.findAll();
-	}
+  @GetMapping("/all")
+  public @ResponseBody Iterable<User> getAllUsers() {
+    return userRepository.findAll();
+  }
 
 }
