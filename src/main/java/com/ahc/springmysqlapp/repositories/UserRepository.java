e51905b9a24e5fdbaa38c042835bfdae4fd67d0d
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-12-14 17:42:32 +0530

    Changed according to Google Java Style Guide.

--- src/main/java/com/ahc/springmysqlapp/repositories/UserRepository.java
+++ src/main/java/com/ahc/springmysqlapp/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package com.ahc.springmysqlapp.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.ahc.springmysqlapp.domain.User;
+
+import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
