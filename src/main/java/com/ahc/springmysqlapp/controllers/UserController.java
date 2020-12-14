e161ea0225c23d8e0b503e0d2cafc6a79a6f35ff
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-12-14 18:31:35 +0530

    Added web interface to display data.

---
+++ src/main/java/com/ahc/springmysqlapp/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.ahc.springmysqlapp.controllers;
+
+import com.ahc.springmysqlapp.repositories.UserRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+  private final UserRepository userRepository;
+
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @RequestMapping({ "", "/", "/index" })
+  public String getUserPage(Model model) {
+    model.addAttribute("users", userRepository.findAll());
+    return "user/index";
+  }
+}
