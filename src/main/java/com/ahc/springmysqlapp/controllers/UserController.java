e1109e2d4ab85ebc346de378c9116a2f45c22971
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-12-24 15:37:38 +0530

    Changed indent

--- src/main/java/com/ahc/springmysqlapp/controllers/UserController.java
+++ src/main/java/com/ahc/springmysqlapp/controllers/UserController.java
@@ -14,41 +14,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/user")
 public class UserController {
 
-  private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-  public UserController(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-  @RequestMapping({ "", "/", "/index" })
-  public String getUserPage(Model model) {
-    model.addAttribute("users", userRepository.findAll());
-    return "user/index";
-  }
+	@RequestMapping({ "", "/", "/index" })
+	public String getUserPage(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "user/index";
+	}
 
-  @GetMapping("/register")
-  public String getUserRegistrationPage() {
-    return "user/register";
-  }
+	@GetMapping("/register")
+	public String getUserRegistrationPage() {
+		return "user/register";
+	}
 
-  /**
-   * Register new user into database.
-   * 
-   * @param name  is user's name
-   * @param email is user's email
-   * @return Spring View
-   */
-  @PostMapping("/register")
-  public String userSubmit(@RequestParam String name, @RequestParam String email, Model model) {
+	/**
+	 * Register new user into database.
+	 * 
+	 * @param name  is user's name
+	 * @param email is user's email
+	 * @return Spring View
+	 */
+	@PostMapping("/register")
+	public String userSubmit(@RequestParam String name, @RequestParam String email, Model model) {
 
-    User user = new User();
-    user.setName(name);
-    user.setEmail(email);
-    userRepository.save(user);
+		User user = new User();
+		user.setName(name);
+		user.setEmail(email);
+		userRepository.save(user);
 
-    model.addAttribute("name", name);
-    model.addAttribute("email", email);
-    return "user/register-result";
-  }
+		model.addAttribute("name", name);
+		model.addAttribute("email", email);
+		return "user/register-result";
+	}
 
 }
--- src/main/java/com/ahc/springmysqlapp/domain/User.java
+++ src/main/java/com/ahc/springmysqlapp/domain/User.java
@@ -8,44 +8,44 @@ import javax.persistence.Id;
 @Entity
 public class User {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 
-  private String name;
-  private String email;
+	private String name;
+	private String email;
 
-  public User(String name, String email) {
-    this.name = name;
-    this.email = email;
-  }
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
 
-  public User() {
+	public User() {
 
-  }
+	}
 
-  public Integer getId() {
-    return id;
-  }
+	public Integer getId() {
+		return id;
+	}
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
